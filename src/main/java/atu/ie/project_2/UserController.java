ba75b5ec2dd4b01f5aa228c9cb0d5894255cc3fd
RakibR7 <G00408644@ATU.IE>
2023-10-15 16:53:45 +0100

    works for exercise 1

--- src/main/java/atu/ie/project_2/UserController.java
+++ src/main/java/atu/ie/project_2/UserController.java
@@ -4,8 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Service
+@RestController
 public class UserController {
     private final UserService userService;
 
